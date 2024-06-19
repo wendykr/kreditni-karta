ff0aee97dc3622013f941a4fa818e1f7bea0e366
Vendula Krajíčková <vendysacek@seznam.cz>
2024-06-19 10:09:12 +0200

    #1 - add: inputMode

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,22 +1,24 @@
-import './Input.scss'
+import "./Input.scss";
 import { forwardRef } from "react";
 
 interface InputProps {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   value: string;
-  number? :boolean;
+  number?: boolean;
 }
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(({ onChange, value, number }, ref) => {
-
-  return (
-    <input
-      className={`input ${number ? 'number' : ''}`}
-      type="text"
-      ref={ref}
-      onChange={onChange}
-      value={value}
-      maxLength={number ? 4 : undefined}
-    />
-  )
-})
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ onChange, value, number }, ref) => {
+    return (
+      <input
+        className={`input ${number ? "number" : ""}`}
+        type="text"
+        inputMode="numeric"
+        ref={ref}
+        onChange={onChange}
+        value={value}
+        maxLength={number ? 4 : undefined}
+      />
+    );
+  }
+);
